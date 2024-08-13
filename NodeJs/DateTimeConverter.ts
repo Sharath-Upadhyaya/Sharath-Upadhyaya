399d91adb5d5b10a549fd08cfa80a02843e75b37
Sharath Upadhyaya <sharath.upadhyaya@gmail.com>
2024-08-13 20:55:23 +0530

    Added cases to handle the various string input.

--- NodeJs/DateTimeConverter.ts
+++ NodeJs/DateTimeConverter.ts
@@ -4,14 +4,92 @@ class DateTimeConverter {
    * @param {string} dateString - The string to convert to a Date object.
    * @returns {Date|null} The Date object or null if conversion fails.
    */
-  static convertStringToDate(dateString) {
+  static ConvertStringToDate(dateString: string): Date | null {
     try {
-      const date = new Date(dateString);
+      let date = new Date(dateString);
+  	  if (isNaN(date.getTime())) {
+  		dateString = dateString.toLowerCase();
+  	    let currentdate = new Date();
+        const currentMonthIndex = currentdate.getMonth(); // 0 for January, 11 for December
+  			
+  		switch(dateString)
+  		{
+  			case "sysdate":
+  			case "sys date":
+  			case "systemdate":
+  			case "system date":
+  			case "currentdatetime":
+  			case "current datetime":
+  			case "currenttime":
+  			case "current time":
+  			case "current":
+  			case "currentdate":
+  			case "date":
+  			case "datetime":
+  			case "time":
+  			case "now":
+  				date = new Date();
+  				break;
+  			case "current date":
+  			case "currentdate":
+  			case "today":
+  				date = new Date(currentdate.getFullYear(), currentdate.getMonth(), currentdate.getDate(), 0, 0, 0, 0);
+  				break;
+  			case "tomorrow":
+  			case "tomorow":
+                  let now = new Date();
+                  const tomorrow = new Date(now); // Create a new Date object based on the current date
+                  tomorrow.setDate(now.getDate() + 1); // Add one day to the current date
+  				date = tomorrow;
+  				break;
+  			case "currentquarterstartdate":
+  			case "current quarter start date":
+  			case "quarterstartdate":
+  			case "quarter start date":
+  				const currentQuarter = Math.floor(currentMonthIndex / 3) + 1;
+  				date = this.GetQuarterStartDate(currentQuarter);
+  				break;
+  			case "nextquarterstartdate":
+  			case "next quarter start date":
+  			case "nextquarterstartdatetime":
+  			case "next quarter start date time":
+  				const nextQuarter = Math.floor(currentMonthIndex / 3) + 2;
+  				date = this.GetQuarterStartDate(nextQuarter);
+  				break;
+  			case "currentweekstartdate":
+  			case "current week start date":
+  			case "weekstartdate":
+  			case "week start date":
+  			case "currentweek":
+  			case "current week":
+  				date = this.GetCurrentWeekStartDate();
+  				break;
+  			case "currentlastdayofweek":
+  			case "current last day of week":
+  			case "lastdayofweek":
+  			case "last day of week":
+  			case "lastdaycurrentweek":
+  			case "lastdayofweek":
+  			case "last day of week":
+  				date = this.GetLastDayOfWeek();
+  				break;
+  			case "currentyearstartdate":
+  			case "current year start date":
+  			case "yearstartdate":
+  			case "year start date":
+  				date = this.GetYearStartDate(currentdate.getFullYear());
+  				break;
+  			case "nextyearstartdate":
+  			case "next year start date":
+  				date = this.GetYearStartDate(currentdate.getFullYear() + 1);
+  				break;
+  		}
+  	  }
       if (isNaN(date.getTime())) {
         throw new Error("Invalid date format");
       }
       return date;
-    } catch (error) {
+    } catch (error:any) {
       console.error("Error converting string to date:", error.message);
       return null;
     }
@@ -21,11 +99,10 @@ class DateTimeConverter {
    * Gets the starting date of the current quarter.
    * @returns {Date} The starting date of the current quarter.
    */
-  static getCurrentQuarterStartDate() {
-    const now = new Date();
-    const currentMonth = now.getMonth();
-    const quarterStartMonth = Math.floor(currentMonth / 3) * 3;
-    return new Date(now.getFullYear(), quarterStartMonth, 1);
+  static GetQuarterStartDate(quarter:number, year:number = new Date().getFullYear()):Date {
+    const quarterEndMonths = [2, 5, 8, 11]; // March, June, September, December
+    const quarterStartMonth = quarterEndMonths[quarter - 2];
+    return new Date(year, quarterStartMonth, 1);
   }
 
   /**
@@ -34,7 +111,7 @@ class DateTimeConverter {
    * @param {number} year - The year for which to get the last day of the quarter.
    * @returns {Date} The last day of the specified quarter.
    */
-  static getLastDayOfQuarter(quarter, year = new Date().getFullYear()) {
+  static GetQuarterLastDay(quarter:number, year:number = new Date().getFullYear()):Date {
     const quarterEndMonths = [2, 5, 8, 11]; // March, June, September, December
     const lastMonthOfQuarter = quarterEndMonths[quarter - 1];
     const lastDay = new Date(year, lastMonthOfQuarter + 1, 0); // Last day of the quarter month
@@ -45,12 +122,12 @@ class DateTimeConverter {
    * Gets the first day of the current week (Monday).
    * @returns {Date} The first day of the current week.
    */
-  static getCurrentWeekStartDate() {
-    const now = new Date();
-    const dayOfWeek = now.getDay(); // 0 (Sunday) to 6 (Saturday)
+  static GetCurrentWeekStartDate():Date {
+    const currentdate = new Date();
+    const dayOfWeek = currentdate.getDay(); // 0 (Sunday) to 6 (Saturday)
     const diffToMonday = (dayOfWeek + 6) % 7; // Calculate difference to get to Monday
-    const firstDayOfWeek = new Date(now);
-    firstDayOfWeek.setDate(now.getDate() - diffToMonday);
+    const firstDayOfWeek = new Date(currentdate);
+    firstDayOfWeek.setDate(currentdate.getDate() - diffToMonday);
     return firstDayOfWeek;
   }
 
@@ -58,19 +135,29 @@ class DateTimeConverter {
    * Gets the last day of the current week (Sunday).
    * @returns {Date} The last day of the current week.
    */
-  static getLastDayOfWeek() {
-    const now = new Date();
-    const dayOfWeek = now.getDay(); // 0 (Sunday) to 6 (Saturday)
+  static GetLastDayOfWeek():Date {
+    const currentdate = new Date();
+    const dayOfWeek = currentdate.getDay(); // 0 (Sunday) to 6 (Saturday)
     const diffToSunday = 6 - dayOfWeek; // Calculate difference to get to Sunday
-    const lastDayOfWeek = new Date(now);
-    lastDayOfWeek.setDate(now.getDate() + diffToSunday);
+    const lastDayOfWeek = new Date(currentdate);
+    lastDayOfWeek.setDate(currentdate.getDate() + diffToSunday);
     return lastDayOfWeek;
+  }
+  
+  /**
+   * Gets the start day of a specified year.
+   * @param {number} year - The year for which to get the last day of the quarter.
+   * @returns {Date} The start day of the specified year.
+   */
+  static GetYearStartDate(year:number = new Date().getFullYear()):Date {
+    const startOfYear = new Date(year, 0, 1); // January is month 0
+    return startOfYear;
   }
 }
 
 // Example usage:
-console.log("Converted Date:", DateTimeConverter.convertStringToDate("2024-08-11T10:30:00"));
-console.log("Current Q1 Starting Date:", DateTimeConverter.getCurrentQuarterStartDate());
-console.log("Last Day of Q2:", DateTimeConverter.getLastDayOfQuarter(2));
-console.log("Current First Day of the Week:", DateTimeConverter.getCurrentWeekStartDate());
-console.log("Last Day of This Week:", DateTimeConverter.getLastDayOfWeek());
+console.log("Converted Date:", DateTimeConverter.ConvertStringToDate("2024-08-11T10:30:00"));
+console.log("Current Q1 Starting Date:", DateTimeConverter.GetQuarterStartDate(1));
+console.log("Last Day of Q2:", DateTimeConverter.GetQuarterLastDay(2));
+console.log("Current First Day of the Week:", DateTimeConverter.GetCurrentWeekStartDate());
+console.log("Last Day of This Week:", DateTimeConverter.GetLastDayOfWeek());
